9b48ff371fc955f26c420f462e79d9253b921286
jeff.yong.huang <jeff.yong.huang@734f2632-fe81-b282-bbc6-629602d6544e>
2010-09-14 06:32:21 +0000

    Created a tiny-examples to contain some minor projects for some simple testing.

---
+++ trunk/tiny-examples/README.txt
@@ -0,0 +1,2 @@
+Projects within this folder is for some simple testing.
+I would like to suggest not to merge into the major tree.
---
+++ trunk/tiny-examples/springside-sub-examples-archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.springside-sub.examples.archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
