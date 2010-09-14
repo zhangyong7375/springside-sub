aa2ea18d5a2890655fdd55a08be4a2b16d69ad77
jeff.yong.huang <jeff.yong.huang@gmail.com>
2010-09-14 11:01:49 +0000

    Demonstrated the three approaches to access the spring beans' container: http://static.springsource.org/spring/docs/2.0.x/reference/beans.html

--- tiny-examples/spring-ioc-test/src/com/springioc/Main.java
+++ tiny-examples/spring-ioc-test/src/com/springioc/Main.java
@@ -5,8 +5,13 @@
 
 package com.springioc;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 /**
  *
@@ -18,16 +23,38 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext(
-                new String[] {"com/springioc/app.xml"});
-        MyBean [] beans = {
-            (MyBean) ctx.getBean("myBean1"),(MyBean) ctx.getBean("myBean2"),(MyBean) ctx.getBean("myBean3"),
-        };
+        usingApplicationContext();
+        usingClassPathResource();
+        usingFileSystemResource();
+    }
 
+    private static void usingApplicationContext() throws BeansException {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[]{"com/springioc/app.xml"});
+        MyBean[] beans = {(MyBean) ctx.getBean("myBean1"), (MyBean) ctx.getBean("myBean2"), (MyBean) ctx.getBean("myBean3")};
+        for (MyBean b : beans) {
+            b.sayHello();
+        }
+    }
+
+    private static void usingClassPathResource() {
+        ClassPathResource resource = new ClassPathResource("com/springioc/app.xml");
+        BeanFactory factory = new XmlBeanFactory(resource);
+        MyBean[] beans = { (MyBean)factory.getBean("myBean1"), (MyBean)factory.getBean("myBean2"),
+            (MyBean)factory.getBean("myBean3") };
         for(MyBean b : beans){
             b.sayHello();
         }
+    }
 
+    private static void usingFileSystemResource() {
+        FileSystemResource resource =
+                new FileSystemResource("src/com/springioc/app.xml");
+        BeanFactory factory = new XmlBeanFactory(resource);
+        MyBean[] beans = { (MyBean)factory.getBean("myBean1"), (MyBean)factory.getBean("myBean2"),
+            (MyBean)factory.getBean("myBean3") };
+        for(MyBean b : beans){
+            b.sayHello();
+        }
     }
 
 }
