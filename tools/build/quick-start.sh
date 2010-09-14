f86b4ad87259088a66381563bfa9947a009056ab
jeff.yong.huang <jeff.yong.huang@734f2632-fe81-b282-bbc6-629602d6544e>
2010-09-14 00:26:17 +0000

    Changed the quick-start.sh 1. Bugfix the central repo target location 2. Created a start_service() function
    Test fails currently because I skipped the h2 database part.
    Will be fixed in the next revision.

--- tools/build/quick-start.sh
+++ tools/build/quick-start.sh
@@ -40,7 +40,8 @@ fi
 
 # Copy the central-repository to user's home
 if [ -d "$CENTRAL_REPO" ] ; then
-	cp -v -rp "${CENTRAL_REPO}" "$HOME/.m2/repository"
+	mkdir -p "$HOME/.m2/repository"
+	cp -v -rp "${CENTRAL_REPO}/*" "$HOME/.m2/repository"
 else
 	info_msg "Skip copying ${CENTRAL_REPO}... Missing"
 fi
@@ -58,32 +59,48 @@ if [ $? -ne 0 ] ; then
 fi
 
 
-info_msg "[Step 3] Configure embedded database"
-cd tools/h2
-mvn ${OFF_LINE} exec:java
-if [ $? -ne 0 ] ; then
-	exit 1
-fi
-cd ${SRCDIR}
+#info_msg "[Step 3] Configure embedded database"
+#cd tools/h2
+#mvn ${OFF_LINE} exec:java
+#if [ $? -ne 0 ] ; then
+#	exit 1
+#fi
+#cd ${SRCDIR}
 
-info_msg "[Step 4] Mini-Service"
-cd examples\mini-service
-ant -f bin/build.xml init-db 
-nohup mvn ${OFF_LINE} -Djetty.port=8084 jetty:run &
+#
+# <description> <folder> <jetty port>
+#
+function start_service(){
+	if [ $# -ne 3 ] ; then
+		error_msg "Invalid parameter number for start_service: $#"
+		exit 1
+	fi
+	local DESC=$1
+	local FOLDER="examples/$2"
+	local JETTY_PORT=$3
+	local BUILD_XML_FILE="${SRCDIR}/$FOLDER/bin/build.xml"
+	local POM_XML_FILE="${SRCDIR}/$FOLDER/pom.xml"
 
-cd ${SRCDIR}
+	info_msg "${DESC}"
 
-info_msg "[Step 5] Mini-Web"
-cd examples\mini-web
-ant -f bin/build.xml init-db 
-nohup mvn ${OFF_LINE} -Djetty.port=8085 jetty:run &
-cd ${SRCDIR}
+	if [ ! -e ${BUILD_XML_FILE} ]; then
+		error_msg "Missing build.xml at ${BUILD_XML_FILE}"
+		exit 1
+	fi
 
-info_msg "[Step 6] Showcase"
-cd examples\showcase
-ant -f bin/build.xml init-db
-nohup mvn ${OFF_LINE} -Djetty.port=8086 jetty:run &
-cd ${SRCDIR}
+	if [ ! -e ${POM_XML_FILE} ] ; then
+		error_msg "Missing pom.xml at ${POM_XML_FILE}"
+		exit 1
+	fi
+	ant -f ${BUILD_XML_FILE} init-db
+	nohup mvn ${OFF_LINE} -f ${POM_XML_FILE} -Djetty.port=${JETTY_PORT} jetty:run &
+}
+
+start_service "[Step 4] Mini-Service" "mini-service" 8084
+exit 1
+
+start_service "[Step 5] Mini-Web" "mini-web" 8085
+start_service "[Step 6] Showcase" "showcase" 8086
 
 info_msg "http://localhost:8084/mini-service"
 info_msg "http://localhost:8085/mini-web"
