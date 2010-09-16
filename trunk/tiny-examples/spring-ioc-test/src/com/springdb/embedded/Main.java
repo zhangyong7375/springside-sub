d706165198847a6dc972d02f6c0f722f210d4d81
jeff.yong.huang@gmail.com <jeff.yong.huang@gmail.com@734f2632-fe81-b282-bbc6-629602d6544e>
2010-09-16 06:14:05 +0000

    Finished the test with embedded database and jdbcTemplate. I can't change it to use derby, or a NullPointException will be there.

---
+++ trunk/tiny-examples/spring-ioc-test/src/com/springdb/embedded/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.springdb.embedded;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+//http://static.springsource.org/spring/docs/3.0.x/spring-framework-reference/html/jdbc.html#jdbc-embedded-database-support
+
+/**
+ *
+ * @author jeff.huang
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        //12.8.2 Creating an embedded database instance using Spring XML
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("com/springdb/embedded/app.xml");
+        JdbcTemplate jdbc = (JdbcTemplate)ctx.getBean("jdbcTemplate");
+        Map<String, Object> map = jdbc.queryForMap("SELECT * FROM a LIMIT 1");
+        for(Entry<String, Object> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+    }
+}
