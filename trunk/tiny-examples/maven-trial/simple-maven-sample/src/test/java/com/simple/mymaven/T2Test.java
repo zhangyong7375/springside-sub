79002dff9ad93803b4094b3a156753990534831a
jeff.yong.huang@gmail.com <jeff.yong.huang@gmail.com@734f2632-fe81-b282-bbc6-629602d6544e>
2010-09-18 23:52:11 +0000

    Updated the simple-maven-sample by using NetBeans. 1. Opened the maven project with NetBeans 2. Added a new Java class T2 3. Created a JUnit test case for T2
    Everything works fine exception JUnit within NetBeans could
    only work with JUnit 3.x

---
+++ trunk/tiny-examples/maven-trial/simple-maven-sample/src/test/java/com/simple/mymaven/T2Test.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.simple.mymaven;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Jeff
+ */
+public class T2Test extends TestCase {
+    
+    public T2Test(String testName) {
+        super(testName);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of main method, of class T2.
+     */
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        T2.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
