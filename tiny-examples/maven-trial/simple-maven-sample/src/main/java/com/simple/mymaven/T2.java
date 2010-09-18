80a3c5748abbf066b6ae6dce649f752784b4aad0
jeff.yong.huang <jeff.yong.huang@gmail.com>
2010-09-18 23:52:11 +0000

    Updated the simple-maven-sample by using NetBeans. 1. Opened the maven project with NetBeans 2. Added a new Java class T2 3. Created a JUnit test case for T2
    Everything works fine exception JUnit within NetBeans could
    only work with JUnit 3.x

---
+++ tiny-examples/maven-trial/simple-maven-sample/src/main/java/com/simple/mymaven/T2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.simple.mymaven;
+
+/**
+ *
+ * @author Jeff
+ */
+public class T2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
