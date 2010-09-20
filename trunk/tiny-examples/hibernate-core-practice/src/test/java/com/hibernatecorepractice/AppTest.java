3211b201eeab4499a6a30d1df65b07c027f93c96
jeff.yong.huang@gmail.com <jeff.yong.huang@gmail.com@734f2632-fe81-b282-bbc6-629602d6544e>
2010-09-20 00:36:29 +0000

    1. Changed junit version from 3.8.1 to 4.8.1 according to springside project. 2. Add the dependency for hibernate according to http://docs.jboss.org/hibernate/stable/core/reference/en/html/tutorial.html#tutorial-firstapp-setup    But the tutorial misses the version for every dependency.    I modified them by following: https://forum.hibernate.org/viewtopic.php?f=1&t=998479 3. Modified the source code version from default to 1.6 according to springside project.

--- trunk/tiny-examples/hibernate-core-practice/src/test/java/com/hibernatecorepractice/AppTest.java
+++ trunk/tiny-examples/hibernate-core-practice/src/test/java/com/hibernatecorepractice/AppTest.java
@@ -1,36 +1,33 @@
 package com.hibernatecorepractice;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.PrintWriter;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public AppTest( )
     {
-        super( testName );
+        
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
+    
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
         assertTrue( true );
