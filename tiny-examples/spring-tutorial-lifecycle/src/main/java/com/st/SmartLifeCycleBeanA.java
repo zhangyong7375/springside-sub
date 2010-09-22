3a5271762746ec156bbe2c3ab141199e695db8b1
jeff.yong.huang <jeff.yong.huang@gmail.com>
2010-09-22 00:54:47 +0000

    Practised with SmartLifecycle interface: 1. isAutoStartup() When the context is refreshed (after all objects have been instantiated and initialized), that callback will be invoked, and at that point the default lifecycle processor will check the boolean value returned by each SmartLifecycle object's isAutoStartup() method. If "true", then that object will be started at that point rather than waiting for an explicit invocation of the context's or its own start() method (unlike the context refresh, the context start does not happen automatically for a standard context implementation).
    src/org/springframework/context/support/AbstractApplicationContext.java
     898     /**
     899      * Finish the refresh of this context, invoking the LifecycleProcessor's
     900      * onRefresh() method and publishing the
     901      * {@link org.springframework.context.event.ContextRefreshedEvent}.
     902      */
     903     protected void finishRefresh() {
     904         // Initialize lifecycle processor for this context.
     905         initLifecycleProcessor();
     906
     907         // Propagate refresh to lifecycle processor first.
     908         getLifecycleProcessor().onRefresh();
     909
     910         // Publish the final event.
     911         publishEvent(new ContextRefreshedEvent(this));
     912     }
    
    
    src/org/springframework/context/support/DefaultLifecycleProcessor.java
    107     public void onRefresh() {
    108         startBeans(true);
    109         this.running = true;
    110     }
    
    122     // internal helpers
    123
    124     private void startBeans(boolean autoStartupOnly) {
    125         Map<String, Lifecycle> lifecycleBeans = getLifecycleBeans();
    126         Map<Integer, LifecycleGroup> phases = new HashMap<Integer, LifecycleGroup>();
    127         for (Map.Entry<String, ? extends Lifecycle> entry : lifecycleBeans.entrySet()) {
    128             Lifecycle lifecycle = entry.getValue();
    129             if (!autoStartupOnly || (lifecycle instanceof SmartLifecycle && ((SmartLifecycle) lifecycle).isAutoS    tartup())) {
    130                 int phase = getPhase(lifecycle);
    131                 LifecycleGroup group = phases.get(phase);
    
    2. getPhase()
    At the other end of the spectrum, a phase value of Integer.MAX_VALUE would indicate that the 
    object should be started last and stopped first (likely because it depends on other processes 
    to be running). When considering the phase value, it's also important to know that the default 
    phase for any "normal" Lifecycle object that does not implement SmartLifecycle would be 0. 
    
    3. stop(callback)
    Not yet.

---
+++ tiny-examples/spring-tutorial-lifecycle/src/main/java/com/st/SmartLifeCycleBeanA.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.st;
+
+import java.util.logging.Logger;
+import org.springframework.context.SmartLifecycle;
+
+/**
+ *
+ * @author jeff.huang
+ */
+public class SmartLifeCycleBeanA implements SmartLifecycle{
+    private static final Logger logger = Logger.getLogger(SmartLifeCycleBeanB.class.getName());
+    private boolean running;
+
+    @Override
+    public boolean isAutoStartup() {
+        return true;
+    }
+
+    @Override
+    public void stop(Runnable callback) {
+        logger.info("stop");
+        running = false;
+    }
+
+    @Override
+    public void start() {
+        logger.info("start");
+        running = true;
+    }
+
+    @Override
+    public void stop() {
+        logger.info("stop");
+        running = false;
+    }
+
+    @Override
+    public boolean isRunning() {
+        logger.info("isRunning " + running);
+        return running;
+    }
+
+    @Override
+    public int getPhase() {
+        return 1;
+    }
+
+}
