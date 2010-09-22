21b4bb2438e2db26e5287c511906df72c0e73524
jeff.yong.huang@gmail.com <jeff.yong.huang@gmail.com@734f2632-fe81-b282-bbc6-629602d6544e>
2010-09-22 02:21:41 +0000

    Practised a simple factory bean: 3.8.3 Customizing instantiation logic with the FactoryBean Interface

--- trunk/tiny-examples/spring-turtorial-container-extension-points/src/main/java/st/cep/App.java
+++ trunk/tiny-examples/spring-turtorial-container-extension-points/src/main/java/st/cep/App.java
@@ -1,5 +1,6 @@
 package st.cep;
 
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -11,6 +12,9 @@ public class App
     public static void main( String[] args )
     {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("app.xml");
-        
+        FactoryBean fb = (FactoryBean)ctx.getBean("&beanFromFactory");
+        System.out.println("Factory Bean = " + fb.getClass().getName() + " : return type = " + fb.getObjectType().getName());
+        MyBean bean = ctx.getBean("beanFromFactory", MyBean.class);
+        System.out.println("My Bean = " + bean.toString());
     }
 }
