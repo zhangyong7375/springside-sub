9e8a17df21cf13297577f5b52b3e2374a7a78bfe
jeff.yong.huang@gmail.com <jeff.yong.huang@gmail.com@734f2632-fe81-b282-bbc6-629602d6544e>
2010-11-29 23:53:20 +0000

    DEMO for schedulor

---
+++ trunk/tiny-examples/demo-schedulor/src/main/java/demo/schedulor/App.java
@@ -0,0 +1,13 @@
+package demo.schedulor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        BeeperControl bc = new BeeperControl();
+        bc.beepForAnHour();
+    }
+}
---
+++ trunk/tiny-examples/demo-schedulor/src/main/java/demo/schedulor/BeeperControl.java
@@ -0,0 +1,51 @@
+/*
+ *  Copyright 2010 jeff.huang.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package demo.schedulor;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author jeff.huang
+ */
+public class BeeperControl {
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    public void beepForAnHour() {
+        final Runnable beeper = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("beep");
+            }
+        };
+
+        final ScheduledFuture<?> beeperHandle =
+                scheduler.scheduleAtFixedRate(beeper, 10, 10, TimeUnit.SECONDS);
+        scheduler.schedule(new Runnable() {
+
+            @Override
+            public void run() {
+                beeperHandle.cancel(true);
+            }
+
+        }, 60 * 60, TimeUnit.SECONDS);
+    }
+}
