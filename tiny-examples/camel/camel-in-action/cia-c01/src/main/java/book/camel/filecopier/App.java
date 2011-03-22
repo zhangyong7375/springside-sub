8e70c56137e77ce6b3cf83e86ee53371a83277c2
jeff.yong.huang <jeff.yong.huang@gmail.com>
2011-03-22 03:34:01 +0000

    Finished the first EndPoint example and created one simple route.

--- tiny-examples/camel/camel-in-action/cia-c01/src/main/java/book/camel/filecopier/App.java
+++ tiny-examples/camel/camel-in-action/cia-c01/src/main/java/book/camel/filecopier/App.java
@@ -1,13 +1,29 @@
 package book.camel.filecopier;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder() {
+
+            @Override
+            public void configure() throws Exception {
+                from("file:data/inbox?noop=true")
+                        .to("file:data/outbox");
+            }
+
+        });
+
+        context.start();
+        Thread.sleep(100000);
+        context.stop();
     }
 }
